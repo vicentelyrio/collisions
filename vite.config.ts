ec14b22cdba2713f26681fe55abfc576f1a57bca
vicentelyrio <vicentelyrio@gmail.com>
2023-10-11 23:53:32 -0300

    chore(pages): unifying steps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: 'collisions',
   plugins: [
     react(),
     tsConfigPaths(),
